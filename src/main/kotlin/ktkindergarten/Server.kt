9132315e65435666f4492d567dcf2a4430e28945
João Antunes <joaofbantunes@gmail.com>
2018-09-09 19:30:48 +0100

    Trying out Kotlin

---
+++ src/main/kotlin/ktkindergarten/Server.kt
@@ -0,0 +1,31 @@
+@file:JvmName("Server")
+
+package ktkindergarten
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.features.ContentNegotiation
+import io.ktor.jackson.jackson
+
+fun main(args: Array<String>) {
+    var requests = 0
+    println("Starting server...")
+    embeddedServer(Netty, 8080) {
+        install(ContentNegotiation) {
+            jackson {}
+        }
+        routing {
+            get("/") {
+                val requestNumber = ++requests
+                call.respond(SomeResponse(
+                        requestNumber,
+                        if (requestNumber == 1) "Hellooooooooooooooo!" else "Hello again - ${requestNumber} requests!"))
+            }
+        }
+    }.start(wait = true)
+}
+
+data class SomeResponse(val requestNumber: Int, val greeting: String)
